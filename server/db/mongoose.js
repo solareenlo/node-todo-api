6fbcffee035515ca888022a382005016b334f6b0
solareenlo <solareenlo@gmail.com>
2018-11-19 05:33:20 +0900

    ちょっと変更

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://solareenlo:beAf2sN9/8NE{V?>NLiLmMTC>g2%3c2x=sRiKcQTzBh47gjFP?=vF62oVAWriQM/@ds221148.mlab.com:21148/murmuring-beach-81397', {useNewUrlParser: true});
 
 module.exports = {mongoose};
