656e6fee05f74840d5d7d4b38df057d3838df4ea
solareenlo <solareenlo@gmail.com>
2018-11-21 01:17:54 +0900

    jwtを使って, データとpaswordからtokenを作って, tokenをデコードする

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,27 +1,37 @@
 const {SHA256} = require('crypto-js');
-
-let message = `I am user number 3.`;
-let hash = SHA256(message).toString();
-
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+const jwt = require('jsonwebtoken');
 
 let data = {
-  id: 4
-};
-let token = {
-  data,
-  hash: SHA256(JSON.stringify(data) + `somesercret`).toString()
+  id: 10
 };
 
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(token.data));
+let token = jwt.sign(data, `123abc`);
+console.log(token);
 
-let resultHash = SHA256(JSON.stringify(data) + `somesercret`).toString();
+let decoded = jwt.verify(token, `123abc`);
+console.log(`decoded: `, decoded);
 
-if (resultHash === token.hash) {
-  console.log(`Data was not changed`);
-} else {
-  console.log(`Data was changed. Do not trust!`);
-};
-
+// let message = `I am user number 3.`;
+// let hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+//
+// let data = {
+//   id: 4
+// };
+// let token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + `somesercret`).toString()
+// };
+//
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data));
+//
+// let resultHash = SHA256(JSON.stringify(data) + `somesercret`).toString();
+//
+// if (resultHash === token.hash) {
+//   console.log(`Data was not changed`);
+// } else {
+//   console.log(`Data was changed. Do not trust!`);
+// };
