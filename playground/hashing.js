46ab97e6ee8e35bfc20334216348d1a7225879c6
solareenlo <solareenlo@gmail.com>
2018-11-22 02:43:18 +0900

    パスワードのハッシュ化と答え合わせの練習

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,33 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-  id: 10
-};
+let password = `123abc`;
 
-let token = jwt.sign(data, `123abc`);
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-let decoded = jwt.verify(token, `123abc`);
-console.log(`decoded: `, decoded);
+let hashedPassword = `$2a$10$5xrMGzCQoTk1f.Ewa.SwFujhjOY./VhEB3Mw3zJ.7m119mojaTvdO`;
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+bcrypt.compare(`123`, hashedPassword, (err, res) => {
+  console.log(res);
+});
+// let data = {
+//   id: 10
+// };
+//
+// let token = jwt.sign(data, `123abc`);
+// console.log(token);
+//
+// let decoded = jwt.verify(token, `123abc`);
+// console.log(`decoded: `, decoded);
 
 // let message = `I am user number 3.`;
 // let hash = SHA256(message).toString();
