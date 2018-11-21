1fd118ff5224154a2d4efa10e68db3dee75eb9fc
solareenlo <solareenlo@gmail.com>
2018-11-22 05:14:18 +0900

    テスト環境でトークンを持つユーザーとトークンを持たないユーザーを作成

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,7 +1,27 @@
 const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
 
+// userのデータを作成
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+const users = [{
+  _id: userOneId,
+  email: 'sola@example.com',
+  password: 'userOnePass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'sola2@example.com',
+  password: 'userTwoPass'
+}];
+
+// todoのデータを作成
 const todos = [{
   _id: new ObjectID(),
   text: 'First test todo'
@@ -18,4 +38,13 @@ const populateTodos = (done) => {
   }).then(() => done());
 };
 
-module.exports = {todos, populateTodos};
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    let userOne = new User(users[0]).save();
+    let userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo])
+  }).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,9 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
-const {todos, populateTodos} = require('./seed/seed');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-
+beforeEach(populateUsers);
 beforeEach(populateTodos);
 
 describe('POST /todos', () => {
