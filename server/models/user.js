0afe8275147f118035e40480af19669beb8ccd24
solareenlo <solareenlo@gmail.com>
2018-11-22 00:27:58 +0900

    POSTされたら、jwtでトークンを作って、ヘッダーにトークンを納めて、返す

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-let User = mongoose.model('User', {
+let UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -29,5 +31,26 @@ let User = mongoose.model('User', {
     }
   }]
 });
+
+UserSchema.methods.toJSON = function () {
+  let user = this;
+  let userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  let user = this;
+  let access = 'auth';
+  let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens = user.tokens.concat([{access, token}]);
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+let User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
